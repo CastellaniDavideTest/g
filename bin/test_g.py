62d95a9af5d40702e277c2dae2fd2993a3539ff0
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-29 10:54:49 +0100

    Created bin/test_g.py

---
+++ bin/test_g.py
@@ -0,0 +1,17 @@
+"""Test g file
+"""
+from g import *
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-29"
+
+def test():
+	"""Tests the g function in the g class
+	Write here all test you want to do.
+	REMEMBER to test your programm you can't use __init__ function
+	"""
+	assert g.g() == "g", "test failed"
+	#assert g.<function>(<values>) == <the result(s) you would like to have>, "<the fail message>"
+	
+if __name__ == "__main__":
+	test()
