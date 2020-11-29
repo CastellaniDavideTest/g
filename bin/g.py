aa30f3f3ea7025807e817829a8142cdd4cc8b748
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-29 10:54:45 +0100

    Created bin/g.py

---
+++ bin/g.py
@@ -0,0 +1,19 @@
+"""g
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-29"
+
+class g:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(g.g())
+	
+	def g():
+		"""g first funtion
+		"""
+		return "g" #for the test (see test_g.py)
+		
+if __name__ == "__main__":
+	g()
